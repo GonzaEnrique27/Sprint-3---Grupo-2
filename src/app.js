1e5a9135d035d5170ffd7cbf66dde5126d25e8e4
kalanchoe18 <marcoaleman18@gmail.com>
2021-11-28 23:24:29 -0300

    vistas parcializadas

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Enrutadores
 var indexRouter = require('./routes/indexRouter');
 var loginRouter = require('./routes/loginRouter');
 var productCartRouter = require('./routes/productCartRouter');
